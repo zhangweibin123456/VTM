1833b9831b5cdf7d671cb3489fcb33e94ec89fbf
maying <maying@maying-PC>
2017-05-04 22:30:22 +0800

    1

--- src/main/java/com/controller/impl/UserControllerImpl.java
+++ src/main/java/com/controller/impl/UserControllerImpl.java
@@ -1,5 +1,6 @@
 package com.controller.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -7,18 +8,21 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bean.UserPO;
 import com.commons.AppStateStore;
 import com.controller.UserController;
 import com.model.UserModel;
 import com.service.UserService;
+import com.utils.Msg;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserControllerImpl implements UserController {
 
@@ -29,6 +33,27 @@ public class UserControllerImpl implements UserController {
 	
 	@Resource(name = "appStateStore")
 	private AppStateStore appStateStore;
+	
+	
+	
+	@ResponseBody
+	@RequestMapping(value="/emp/{ids}",method=RequestMethod.DELETE)
+	public Msg deleteEmp(@PathVariable("ids")String ids){
+		//批量删除
+		if(ids.contains("-")){
+			List<Integer> del_ids = new ArrayList<>();
+			String[] str_ids = ids.split("-");
+			//组装id的集合
+			for (String string : str_ids) {
+				del_ids.add(Integer.parseInt(string));
+			}
+			//employeeService.deleteBatch(del_ids);
+		}else{
+			Integer id = Integer.parseInt(ids);
+			//employeeService.deleteEmp(id);
+		}
+		return Msg.success();
+	}
 
 	@RequestMapping("/findAll")
 	public List<UserPO> findAll(@RequestBody UserModel userModel) {
